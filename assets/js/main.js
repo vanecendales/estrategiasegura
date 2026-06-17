15e996696b3369a3107f9c5c57745c33a664d802
Vanessa Cendales <vanecendales@gmail.com>
2026-06-17 09:08:08 -0500

    dashboard pixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 $(function() {
     $('#sidebar-toggle').on('click', function() {
-        $('#sidebar').toggleClass('open');
-        $('#sidebar-overlay').toggleClass('open');
-    });
-
-    $('#sidebar-overlay').on('click', function() {
-        $('#sidebar').removeClass('open');
-        $(this).removeClass('open');
+        const $sidebar = $('#sidebar');
+        if ($sidebar.hasClass('collapsed')) {
+            $sidebar.removeClass('collapsed');
+            $('#sidebar-toggle-icon').removeClass('bi-chevron-right').addClass('bi-chevron-left');
+        } else {
+            $sidebar.addClass('collapsed');
+            $('#sidebar-toggle-icon').removeClass('bi-chevron-left').addClass('bi-chevron-right');
+        }
     });
 });
