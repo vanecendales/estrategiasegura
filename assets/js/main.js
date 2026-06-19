0e5e1c6cec8ba168c58cc65b4d80bf6cbe39733c
Vanessa Cendales <vanecendales@gmail.com>
2026-06-19 07:29:15 -0500

    historic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,28 @@
 $(function() {
-    $('#sidebar-toggle').on('click', function() {
-        const $sidebar = $('#sidebar');
+    const $sidebar = $('#sidebar');
+    const $toggleBtn = $('#sidebar-toggle');
+    const $toggleIcon = $('#sidebar-toggle-icon');
+
+    if ($(window).width() < 768) {
+        $sidebar.addClass('collapsed');
+        $toggleIcon.removeClass('bi-chevron-left').addClass('bi-chevron-right');
+    }
+
+    $toggleBtn.on('click', function(e) {
+        e.preventDefault();
+        $sidebar.toggleClass('collapsed');
+
         if ($sidebar.hasClass('collapsed')) {
-            $sidebar.removeClass('collapsed');
-            $('#sidebar-toggle-icon').removeClass('bi-chevron-right').addClass('bi-chevron-left');
+            $toggleIcon.removeClass('bi-chevron-left').addClass('bi-chevron-right');
         } else {
-            $sidebar.addClass('collapsed');
-            $('#sidebar-toggle-icon').removeClass('bi-chevron-left').addClass('bi-chevron-right');
+            $toggleIcon.removeClass('bi-chevron-right').addClass('bi-chevron-left');
         }
+    });
+});
+
+document.getElementById('filtro-tipo').addEventListener('change', function () {
+    const valor = this.value;
+    document.querySelectorAll('#lista-adjuntos .card[data-tipo]').forEach(function (card) {
+        card.classList.toggle('d-none', valor !== '' && card.dataset.tipo !== valor);
     });
 });
